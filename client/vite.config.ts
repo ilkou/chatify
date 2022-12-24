d06fc8d8fd3e92cfc40f9d87a90ca384cf053102
ilkou <ouklichoussama@gmail.com>
2022-12-24 22:30:23 +0100

    fix app base

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "/chatify/",
   server: {
     proxy: {
-      '/api': 'https://learn-pb-prod-pocket-di0czo.mo6.mogenius.io',
+      "/api": "https://learn-pb-prod-pocket-di0czo.mo6.mogenius.io",
     },
   },
-})
+});
