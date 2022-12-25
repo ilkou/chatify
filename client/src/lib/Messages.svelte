e17360420438136f14ddd0767a5d8cd9a0ab74ba
ilkou <ouklichoussama@gmail.com>
2022-12-25 22:44:13 +0100

    load last messages, then previous ones

--- client/src/lib/Messages.svelte
+++ client/src/lib/Messages.svelte
@@ -6,8 +6,12 @@
   let newMessage: string;
   let unsubscribe: () => void;
   let messages = [];
+  let orderedMessages;
   let div;
   let autoscroll;
+  let hasLoadedEverything = false;
+  let loadingOldMessages = false;
+  let sizeOfMessages = 15;
 
   beforeUpdate(() => {
     autoscroll =
@@ -18,11 +22,20 @@
     if (autoscroll) div.scrollTo(0, div.scrollHeight);
   });
 
-  onMount(async () => {
-    messages = await pb.collection("messages").getFullList(50, {
-      sort: "created",
+  async function loadMessages(pageSize) {
+    const response = await pb.collection("messages").getList(1, pageSize, {
+      sort: "-created",
       expand: "user",
     });
+    if (response.totalItems == response.items.length) {
+      hasLoadedEverything = true;
+    }
+    messages = response.items;
+    loadingOldMessages = false;
+  }
+
+  onMount(async () => {
+    await loadMessages(sizeOfMessages);
 
     unsubscribe = await pb
       .collection("messages")
@@ -30,7 +43,7 @@
         if (action === "create") {
           const user = await pb.collection("users").getOne(record.user);
           record.expand = { user };
-          messages = [...messages, record];
+          messages = [record, ...messages];
         }
         if (action === "delete") {
           messages = messages.filter((message) => message.id !== record.id);
@@ -48,12 +61,26 @@
     });
     newMessage = "";
   }
+
+  $: orderedMessages = [...messages].reverse();
+
+  async function loadMoreOnTopChat(e) {
+    if (e.target.scrollTop === 0) {
+      loadingOldMessages = true;
+      await loadMessages(sizeOfMessages + 4);
+      sizeOfMessages = sizeOfMessages + 2;
+      if (!hasLoadedEverything) {
+        div.scrollTo(0, 30);
+      }
+    }
+  }
 </script>
 
 <br />
 
-<div class="chat-container" bind:this={div}>
-  {#each messages as message, i (message.id)}
+<div class="chat-container" bind:this={div} on:scroll={loadMoreOnTopChat}>
+  {loadingOldMessages && !hasLoadedEverything ? "Loading old Messages..." : ""}
+  {#each orderedMessages as message, i (message.id)}
     <div>
       <span style={getUsernameStyles(message.expand?.user?.id)}>
         {message.expand?.user?.username}:
