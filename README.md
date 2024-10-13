d7efb2954218eff460b9944ab93002e1259d5277
ilkou <48165230+ilkou@users.noreply.github.com>
2024-10-13 16:28:12 +0100

    run locally

---
+++ README.md
@@ -0,0 +1,12 @@
+# learn pb
+
+build docker image:
+```bash
+docker build -t ilkou/learn-pb .
+```
+
+then run it:
+```bash
+docker compose up -d
+```
+access pb admin UI: [http://localhost:8088/_/](http://localhost:8088/_/)
--- client/src/lib/pocketbase.ts
+++ client/src/lib/pocketbase.ts
@@ -3,7 +3,8 @@ import Pocketbase from "pocketbase";
 import { writable } from "svelte/store";
 
 export const pb = new Pocketbase(
-  "https://learn-pb-prod-pocket-di0czo.mo6.mogenius.io"
+  "http://localhost:8088"
+  // "https://learn-pb-prod-pocket-di0czo.mo6.mogenius.io"
 );
 
 export const currentUser = writable(pb.authStore.model);
